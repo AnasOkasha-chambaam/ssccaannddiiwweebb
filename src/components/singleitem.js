d9f62b6cbfad1617daf023873c11ed25825cee18
AnasOkasha-chambaam <anasokashachama@gmail.com>
2022-10-18 14:03:21 +0200

    m

--- src/components/singleitem.js
+++ src/components/singleitem.js
@@ -3,25 +3,41 @@ import { Link } from "react-router-dom";
 import AddToCartIcon from "./addtocarticon";
 
 export default class SingleItem extends Component {
+  constructor(props) {
+    super(props);
+    this.setItemValues = this.setItemValues.bind(this);
+  }
+  /**
+   * Set the attribuites of the object to the first choice of each one.
+   * It happens before adding the item to cart
+   */
+  setItemValues() {
+    let selectedAttributes = {};
+    this.props.categoryItem.attributes.forEach((attribute) => {
+      selectedAttributes[attribute.name] = attribute.items[0].value;
+    });
+    // this.setState({ ...this.state, currentValsObj: selectedAttributes });
+    return selectedAttributes;
+  }
   render() {
     let currentCurrencySymbol = this.props.currentCurrency.split(" ")[0];
     return (
       <Link to={"/iteminfo/" + this.props.categoryItem.id}>
         <li
-          className="item-cart"
-          style={{
-            // backgroundColor: "green",
-            minWidth: "333px",
-            height: "470px",
-            padding: "16px",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            transitionProperty: "box-shadow",
-            transitionDuration: "0.3s",
-            transitionTimingFunction: "ease-in-out",
-            position: "relative",
-          }}
+          className="item-cart single-item"
+          // style={{
+          //   // backgroundColor: "green",
+          //   minWidth: "333px",
+          //   height: "470px",
+          //   padding: "16px",
+          //   display: "flex",
+          //   flexDirection: "column",
+          //   justifyContent: "space-between",
+          //   transitionProperty: "box-shadow",
+          //   transitionDuration: "0.3s",
+          //   transitionTimingFunction: "ease-in-out",
+          //   position: "relative",
+          // }}
         >
           <div
             className={
@@ -30,20 +46,27 @@ export default class SingleItem extends Component {
             }
             onClick={(e) => {
               e.preventDefault();
-              console.log("Toggle");
               if (!this.props.categoryItem.inStock) return;
-              console.log(this.props.onCart);
+              // console.log(this.props.onCart);
               if (!this.props.onCart) {
+                /**
+                 * Set the attribuites of the object to the first choice of each one.
+                 * It happens before adding the item to cart
+                 */
+                let currentValsObj = this.setItemValues();
+                console.log(currentValsObj);
                 let selectedAtt = [],
-                  attNames = Object.keys(this.state.currentValsObj).sort();
+                  attNames = Object.keys(currentValsObj).sort();
                 attNames.forEach((name) => {
-                  selectedAtt.push(this.state.currentValsObj[name]);
+                  selectedAtt.push(currentValsObj[name]);
                 });
+                console.log(selectedAtt);
                 // this.props.addSelectedAttToItemInfo(JSON.stringify(selectedAtt));
-                this.props.addSelectedAttToItemInfo(this.state.currentValsObj);
+                // this.props.addSelectedAttToItemInfo(this.state.currentValsObj);
                 return;
               }
               this.props.deleteTheItem(this.props.item.id);
+              console.log("Toggle");
             }}
           >
             <AddToCartIcon cartItemsNumber={false} />
